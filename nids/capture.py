ba9225c618dcb72e56bd4fbda89400e9562c12cf
Vasundhara Ravi <vasundhararavi641@gmail.com>
2026-06-09 16:15:41 +0200

    feat: wrote down what the purpose of the capture.py file is and the arp_spoof.py file in comments to differentiate responsibilities

---
+++ nids/capture.py
@@ -0,0 +1,3 @@
+# this is the file that will be used for live packet capture
+# It only contains the code for live packet capture, and will be used in the main.py file
+# It is not going to be used to detect the type of attack, but simply captures packets
