2073c4dd7029b8e7aa50c4f9277ff84b00f688fe
Vasundhara Ravi <vasundhararavi641@gmail.com>
2026-06-09 17:20:36 +0200

    Added arp spoofing detection logic to arp_spoof.py

--- nids/rules/arp_spoof.py
+++ nids/rules/arp_spoof.py
@@ -3,3 +3,35 @@
 # It will use the scapy library to capture ARP packets and analyze them for signs of spoofing
 # No other types of attacks will be detected in this file, it will only focus on ARP spoofing detection and also packet capture happens in capture.py
 # this file is used to examine capture.py and see if it matches ARP spoofing attack patterns, and if it does, it will raise an alert
+
+# contains the IP to MAC mapping of the network, and will be updated as new ARP packets are captured
+
+# imports
+from scapy.all import ARP
+
+
+# define the MAC table in the form of a dictionary, where the key is the IP address and the value is the MAC address
+mac_table = {}
+
+# detection logic - is this MAC address associated with this IP address in the MAC table? If not, raise an alert. Trusts only the first address it sees
+def detect_arp_spoof(packet):
+    if not packet.haslayer("ARP"):
+        return
+
+    arp_layer = packet.getlayer("ARP")
+
+    # only analyse replies since that's where the attack happens, and ignore requests
+    if arp_layer.op != 2:
+        return
+        
+    ip_address = arp_layer.psrc
+    mac_address = arp_layer.hwsrc
+
+    if ip_address in mac_table:
+        if mac_table[ip_address] != mac_address:
+            # alert call if MAC address associated with the IP address has changed
+            print(f"ALERT: ARP spoofing detected! IP: {ip_address} is associated with MAC: {mac_table[ip_address]} but now is associated with MAC: {mac_address}")
+        else:
+            # new IP address, add it to the MAC table
+            mac_table[ip_address] = mac_address
+
